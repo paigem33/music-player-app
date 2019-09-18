a21549554cee8c709cd7860a22d9c1d9337899b5
paigem33 <paigemacgregor33@gmail.com>
2019-09-18 13:24:49 -0700

    added styles to progress bar

--- index.php
+++ index.php
@@ -18,13 +18,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
 </head> 
     <div id="nowPlaying">
         <div id="nowPlayingBar">
             <div id="nowPlayingLeft">
-
+                <div class="content contentLeft">
+                    <img src="assets/images/album.png" alt="">
+                    <div class="trackInfo">
+                        <span class="trackName">Ch-Check It Out</span>
+                        <span class="artistName">Beastie Boys</span>
+                    </div>
+                </div>
             </div>
             <div id="nowPlayingCenter">
                 <div class="content playerControls">
@@ -39,14 +46,23 @@
                     <div class="playbackBar">
                         <span class="progressTime current">0:00</span>
                         <div class="progressBar">
-
+                            <div class="progressBg">
+                                <div class="progress"></div>
+                            </div>
                         </div>
                         <span class="progressTime remaining">0:00</span>
                     </div>
                 </div>
             </div>
             <div id="nowPlayingRight">
-                
+                <div class="volumeBar">
+                    <i class="fas fa-volume-up"></i>
+                    <div class="progressBar">
+                        <div class="progressBg">
+                            <div class="progress"></div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
