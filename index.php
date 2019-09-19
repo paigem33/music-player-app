7ac78f41f17bc698060bcefb191c3d3ac94bd595
paigem33 <paigemacgregor33@gmail.com>
2019-09-18 19:52:38 -0700

    finished moble view for now playing bar

--- index.php
+++ index.php
@@ -21,6 +21,8 @@
     <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="assets/scripts/index.js"></script>
 </head> 
     <div id="nowPlaying">
         <div id="nowPlayingBar">
